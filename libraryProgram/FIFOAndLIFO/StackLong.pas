7a60572ae03c5d1d0e67543fe3e7739c4a6e3781
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-07-06 15:35:30 +0300

    Begin interface for stack longint

---
+++ libraryProgram/FIFOAndLIFO/StackLong.pas
@@ -0,0 +1,24 @@
+Program StackLongint;
+
+type 
+	StackOfLongints = ;
+
+procedure SOLinit(var stack:StackOfLongints);
+begin
+
+end;
+
+procedure SOLPush(var stack:StackOfLongints; n:longint);
+begin
+
+end;
+
+function SOLPop(var stack:StackOfLongints; var n:longint):boolean;
+begin
+
+end;
+
+function SOLIsEmpty(var stack:StackOfLongints):boolean;
+begin
+
+end;
