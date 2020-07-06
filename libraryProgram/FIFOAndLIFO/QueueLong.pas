986c7dc6b19ac5144a6e551e6d218e5f11e2c6a1
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-07-06 17:22:53 +0300

    Realized queue for longint

--- libraryProgram/FIFOAndLIFO/QueueLong.pas
+++ libraryProgram/FIFOAndLIFO/QueueLong.pas
@@ -1,27 +1,75 @@
 Program Queue;
-type 
-	QueueOfLongints = ;
+type
+       LongItemPtr = ^LongItem;
+	LongItem = record
+		data:longint;
+		next:LongItemPtr;
+		end;	
+	QueueOfLongints = record 
+		first,last:LongItemPtr;
+		end;
 
-procedure QOLinit(var queue: QueueOfLpngints);
+procedure QOLinit(var queue: QueueOfLongints);
 begin
-
+	queue.first:=nil;
+	queue.last:=nil;
 end;
 
 procedure QOLPut(var queue:QueueOfLongints; n:longint);
 begin
-
+	if queue.first = nil then
+	begin
+		new(queue.first);
+		queue.last :=queue.first;
+	end
+	else
+	begin
+		new(queue.last^.next);
+		queue.last:=queue.last^.next;
+	end;
+	queue.last^.data:=n;
+	queue.last^.next:=nil;
 end;
 
-function QOLGet(var queue:QueueOfLongints; var n:longint);
+function QOLGet(var queue:QueueOfLongints; var n:longint):boolean;
+var 
+	tmp:LongItemPtr;
 begin
-
+	if queue.first = nil then
+	begin
+		QOLGet :=false;
+		exit;
+	end;
+	n:=queue.first^.data;
+	tmp:=queue.first;
+	queue.first :=queue.first^.next;
+	if queue.first = nil then
+		queue.last:=nil;
+	dispose(tmp);
+	QOLGet :=true;
 end;
 
 function QOlIsEmpty(var queue:QueueOfLongints):boolean;
 begin
-
+	QOLIsEmpty :=queue.first = nil;
 end;
 
-begin
+var 
+	q:QueueOfLongints;
+	n:longint;
 
+begin
+	QOLInit(q);
+	while not eof do
+	begin
+		readln(n);
+		QOLPut(q,n);
+	end;
+	Writeln('Get element in queue');
+	while not QOLIsEmpty(q) do
+	begin
+		QOLGet(q,n);
+		Writeln(n);
+	end;
+	
 end.
