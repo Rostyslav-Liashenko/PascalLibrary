2e5b4e608fab8176dc3c33ee378bbf3d00a5140e
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-07-06 16:31:08 +0300

    Ready interface for Queue

---
+++ libraryProgram/FIFOAndLIFO/QueueLong.pas
@@ -0,0 +1,27 @@
+Program Queue;
+type 
+	QueueOfLongints = ;
+
+procedure QOLinit(var queue: QueueOfLpngints);
+begin
+
+end;
+
+procedure QOLPut(var queue:QueueOfLongints; n:longint);
+begin
+
+end;
+
+function QOLGet(var queue:QueueOfLongints; var n:longint);
+begin
+
+end;
+
+function QOlIsEmpty(var queue:QueueOfLongints):boolean;
+begin
+
+end;
+
+begin
+
+end.
