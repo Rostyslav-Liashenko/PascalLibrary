9676d8f2bf97fdcf646a42a7ec4b287bf1a60f33
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-09-08 03:13:30 +0300

    add Basic Logic and upgrade interface

--- Game_Pascal_Lazarus/Tic_Tac_Toe/backup/unit1.pas
+++ Game_Pascal_Lazarus/Tic_Tac_Toe/backup/unit1.pas
@@ -12,8 +12,8 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
-    Button1: TButton;
     Image1: TImage;
+    Image10: TImage;
     Image2: TImage;
     Image3: TImage;
     Image4: TImage;
@@ -26,8 +26,18 @@ type
     Label2: TLabel;
     Label3: TLabel;
     procedure FormCreate(Sender: TObject);
+    procedure Image10Click(Sender: TObject);
+    procedure Image2Click(Sender: TObject);
+    procedure Image3Click(Sender: TObject);
+    procedure Image4Click(Sender: TObject);
+    procedure Image5Click(Sender: TObject);
+    procedure Image6Click(Sender: TObject);
+    procedure Image7Click(Sender: TObject);
+    procedure Image8Click(Sender: TObject);
+    procedure Image9Click(Sender: TObject);
     {my procedure and funtion}
     procedure ShowPlayer;
+    procedure BasicLogic(nom_pic:integer);
   private
 
   public
@@ -43,7 +53,7 @@ implementation
 
 { TForm1 }
 
-procedure ShowPlayer;
+procedure TForm1.ShowPlayer;
 begin
   if player = 0 then
        Label3.caption:='First'
@@ -51,15 +61,89 @@ begin
        Label3.caption:='Second';
 end;
 
+procedure TForm1.BasicLogic(nom_pic:integer);
+begin
+   if player = 0 then
+   begin
+        TImage(FindComponent('Image'+ IntToStr(nom_pic))).Picture.LoadFromFile('new_x.png');
+        TImage(FindComponent('Image'+IntToStr(nom_pic))).Enabled:=false;
+        ShowPlayer;
+        Inc(player);
+   end
+   else
+   begin
+       TImage(FindComponent('Image'+ IntToStr(nom_pic))).Picture.LoadFromFile('new_o.png');
+       TImage(FindComponent('Image'+IntToStr(nom_pic))).Enabled:=false;
+       showPlayer;
+       Dec(player)
+   end;
+end;
+
 procedure TForm1.FormCreate(Sender: TObject);
 var i:integer;
 begin
-  for i:=1 to 9 do
+  player:=0;
+  ShowPlayer;
+  for i:=2 to 10 do
   begin
+    TImage(FindComponent('Image'+IntToStr(i))).Picture.Clear;
     TImage(FindComponent('Image'+IntToStr(i))).Enabled:=true;
   end;
 end;
 
+procedure TForm1.Image2Click(Sender: TObject);
+begin
+  BasicLogic(2);
+  ShowPlayer;
+end;
+
+procedure TForm1.Image3Click(Sender: TObject);
+begin
+  BasicLogic(3);
+  ShowPlayer;
+end;
+
+procedure TForm1.Image4Click(Sender: TObject);
+begin
+  BasicLogic(4);
+  ShowPlayer;
+end;
+
+procedure TForm1.Image5Click(Sender: TObject);
+begin
+  BasicLogic(5);
+  ShowPlayer;
+end;
+
+procedure TForm1.Image6Click(Sender: TObject);
+begin
+  BasicLogic(6);
+  ShowPlayer;
+end;
+
+procedure TForm1.Image7Click(Sender: TObject);
+begin
+  BasicLogic(7);
+  ShowPlayer;
+end;
+
+procedure TForm1.Image8Click(Sender: TObject);
+begin
+   BasicLogic(8);
+   ShowPlayer;
+end;
+
+procedure TForm1.Image9Click(Sender: TObject);
+begin
+   BasicLogic(9);
+   ShowPlayer;
+end;
+
+procedure TForm1.Image10Click(Sender: TObject);
+begin
+   BasicLogic(10);
+   ShowPlayer;
+end;
 
 end.
 
