a66aa4126209b9d3b2e98cd9524e9c6e606f28be
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-09-10 21:41:15 +0300

    Upgrade Interface

--- Game_Pascal_Lazarus/Ping_pong/backup/unit1.pas
+++ Game_Pascal_Lazarus/Ping_pong/backup/unit1.pas
@@ -5,22 +5,30 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, types;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  types;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Panel1: TPanel;
     Shape1: TShape;
     Shape2: TShape;
     Shape3: TShape;
     Timer1: TTimer;
     Timer2: TTimer;
+    procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
     procedure Timer1Timer(Sender: TObject);
     procedure Timer2Timer(Sender: TObject);
+    procedure EndGame(s:string);
   private
 
   public
@@ -36,14 +44,31 @@ implementation
 
 { TForm1 }
 
+
+procedure TForm1.EndGame(s:string);
+begin
+    Shape1.Top:=200;
+    Shape1.Left:=200;
+    Velx:=0;
+    Vely:=0;
+    Label1.Caption:='Winner ' + s;
+    Button1.Enabled:=true;
+end;
+
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  PosX:=50;
+  PosX:=150;
   PosY:=50;
   VelX:=1;
   VelY:=2;
+  Button1.Enabled:=false;
   Randomize;
-  Sleep(1000);
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  FormCreate(sender);
+  Label1.Caption:='';
 end;
 
 procedure TForm1.FormMouseMove(Sender: TObject; Shift: TShiftState; X,
@@ -72,18 +97,18 @@ begin
   Shape1.Top:=round(Posy);
   if InterSectRect(Overlay, Shape2.BoundsRect, Shape1.BoundsRect) then
   begin
+       ShowMessage(FloatToStr(Velx));
+       ShowMessage(FloatTOStr(VelY));
        VelX:=-VelX - Random(5);
        VelY:=-VelY - Random(5);
   end;
   if (Shape1.Left <= 0) then
      begin
-          ShowMessage('Player Winner');
-          Close;
+          EndGame('Player');
      end;
   if (Shape1.Left > Shape2.Left) then
      begin
-          ShowMessage('Computer Winner');
-          Close;
+          EndGame('Computer');
      end;
 end;
 
--- Game_Pascal_Lazarus/Ping_pong/unit1.pas
+++ Game_Pascal_Lazarus/Ping_pong/unit1.pas
@@ -5,22 +5,30 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, types;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  types;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Panel1: TPanel;
     Shape1: TShape;
     Shape2: TShape;
     Shape3: TShape;
     Timer1: TTimer;
     Timer2: TTimer;
+    procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
     procedure Timer1Timer(Sender: TObject);
     procedure Timer2Timer(Sender: TObject);
+    procedure EndGame(s:string);
   private
 
   public
@@ -30,20 +38,39 @@ type
 var
   Form1: TForm1;
   PosX,PosY,VelX,VelY:Single;
+  count:integer;
 implementation
 
 {$R *.lfm}
 
 { TForm1 }
 
+
+procedure TForm1.EndGame(s:string);
+begin
+    Shape1.Top:=200;
+    Shape1.Left:=200;
+    Velx:=0;
+    Vely:=0;
+    Label1.Caption:='Winner ' + s;
+    Button1.Enabled:=true;
+end;
+
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-  PosX:=50;
+  PosX:=150;
   PosY:=50;
   VelX:=1;
   VelY:=2;
+  Button1.Enabled:=false;
+  count:=0;
   Randomize;
-  Sleep(1000);
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  FormCreate(sender);
+  Label1.Caption:='';
 end;
 
 procedure TForm1.FormMouseMove(Sender: TObject; Shift: TShiftState; X,
@@ -55,7 +82,6 @@ end;
 procedure TForm1.Timer1Timer(Sender: TObject);
 var
   Overlay:TRect;
-  RandomNumber:integer;
 begin
   PosX:=PosX + VelX;
   PosY:=PosY + VelY;
@@ -73,18 +99,17 @@ begin
   Shape1.Top:=round(Posy);
   if InterSectRect(Overlay, Shape2.BoundsRect, Shape1.BoundsRect) then
   begin
+    if
        VelX:=-VelX - Random(5);
        VelY:=-VelY - Random(5);
   end;
   if (Shape1.Left <= 0) then
      begin
-          ShowMessage('Player Winner');
-          Close;
+          EndGame('Player');
      end;
   if (Shape1.Left > Shape2.Left) then
      begin
-          ShowMessage('Computer Winner');
-          Close;
+          EndGame('Computer');
      end;
 end;
 
