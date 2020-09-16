dd96a4628ca24a5ab096b7e924fd5c986431f963
Rostyslav Liashenko <kivi27072002@gmail.com>
2020-09-16 20:16:34 +0300

    Edit day_11, added new features

--- Practic_Pascal_Lazarus/Day_11/New_Organization/backup/unit1.pas
+++ Practic_Pascal_Lazarus/Day_11/New_Organization/backup/unit1.pas
@@ -10,11 +10,19 @@ uses
 type
 
   { TForm1 }
+  Tprofit = array[0..100] of Real;
+  Tyears = array[0..100] of Integer;
 
   TForm1 = class(TForm)
     Button1: TButton;
     Button10: TButton;
     Button11: TButton;
+    Button12: TButton;
+    Button13: TButton;
+    Button14: TButton;
+    Button15: TButton;
+    Button16: TButton;
+    Button17: TButton;
     Button2: TButton;
     Button3: TButton;
     Button4: TButton;
@@ -28,6 +36,12 @@ type
     Label1: TLabel;
     Label10: TLabel;
     Label11: TLabel;
+    Label12: TLabel;
+    Label13: TLabel;
+    Label14: TLabel;
+    Label15: TLabel;
+    Label16: TLabel;
+    Label17: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
@@ -39,12 +53,28 @@ type
     Memo1: TMemo;
     Memo2: TMemo;
     Memo3: TMemo;
+    Memo4: TMemo;
+    Memo5: TMemo;
+    Memo6: TMemo;
+    Memo7: TMemo;
+    Memo8: TMemo;
+    Memo9: TMemo;
     Panel1: TPanel;
     Panel2: TPanel;
     Panel3: TPanel;
     Panel4: TPanel;
+    Panel6: TPanel;
+    Panel7: TPanel;
+    Panel8: TPanel;
+    Panel9: TPanel;
     procedure Button10Click(Sender: TObject);
     procedure Button11Click(Sender: TObject);
+    procedure Button12Click(Sender: TObject);
+    procedure Button13Click(Sender: TObject);
+    procedure Button14Click(Sender: TObject);
+    procedure Button15Click(Sender: TObject);
+    procedure Button16Click(Sender: TObject);
+    procedure Button17Click(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
@@ -63,9 +93,10 @@ type
 
 var
   Form1: TForm1;
-  profit:array[0..100] of Real;
-  years:array[0..100] of integer;
+  profit:Tprofit;
+  years:Tyears;
   countElements:integer;
+  MaxSpending:integer;
 implementation
 
 {$R *.lfm}
@@ -88,6 +119,135 @@ begin
  label11.caption:='';
 end;
 
+procedure TForm1.Button12Click(Sender: TObject);
+var i:integer;
+begin
+     Button6Click(Sender);
+     for i:=MaxSpending to CountElements -2 do
+     begin
+          profit[i]:=profit[i+1];
+          years[i]:=years[i+1];
+     end;
+     Dec(CountElements);
+end;
+
+procedure TForm1.Button13Click(Sender: TObject);
+var
+    i:integer;
+    j:integer;
+begin
+  if Memo4.Lines.Count = 0 then
+  begin
+       Application.MessageBox('Fields is empty','Errors');
+       exit;
+  end;
+  if Memo4.Lines.Count <> Memo5.Lines.Count then
+  begin
+       Application.MessageBox('Fields not fill','Error add data');
+       exit;
+  end;
+  for i:=0 to Memo4.Lines.Count do
+  begin
+       if StrToInt(Memo4.Lines[i]) > 2020 then
+       begin
+            Application.MessageBox('Years is not correct','Error add Year');
+            exit;
+       end;
+  end;
+  j:=0;
+  for i:=countElements to countElements + Memo4.Lines.Count - 1 do
+  begin
+       profit[i]:=StrToFloat(Memo5.Lines[j]);
+       years[i]:=StrToInt(Memo4.Lines[j]);
+       Inc(j);
+  end;
+  countElements:=CountElements+Memo4.Lines.Count;
+  Memo4.Clear;
+  Memo5.Clear;
+end;
+
+procedure TForm1.Button14Click(Sender: TObject);
+var i:integer;
+begin
+  Memo6.clear;
+  Memo7.clear;
+  for i:=0 to CountElements-1 do
+  begin
+   Memo6.Lines.add(IntToStr(years[i]));
+   Memo7.Lines.add(FLoatToStrf(profit[i],ffFixed,8,2));
+  end;
+end;
+
+procedure TForm1.Button15Click(Sender: TObject);
+var t2:real;
+begin
+   t2:=profit[CountElements -1 ];
+   profit[CountElements - 1] := profit[0];
+   profit[0]:=t2;
+end;
+
+procedure TForm1.Button16Click(Sender: TObject);
+var count,max,i:integer;
+begin
+     max:=0;
+     count:=0;
+     for i:=0 to CountElements - 1 do
+     begin
+          if (profit[i] <= 0) then
+          begin
+               Inc(count);
+               if count > max then
+                  max:=count;
+          end
+          else
+          begin
+               count:=0;
+          end;
+     end;
+     Label16.Caption:=IntTOStr(count);
+end;
+
+procedure TForm1.Button17Click(Sender: TObject);
+var
+    i,j:integer;
+
+    money:Tprofit;
+    date:TYears;
+
+    count:integer;
+    t:integer;
+    ft:real;
+begin
+  date:=years;
+  money:=profit;
+  count:=CountElements - 1;
+
+  for i:=0 to count - 1 do
+  begin
+       for j:=0 to count - 1 do
+       begin
+            if money[j] > money[j+1] then
+            begin
+                 ft:=money[j];
+                 money[j]:=money[j+1];
+                 money[j+1]:=ft;
+
+                 t := date[j];
+                 date[j]:=date[j+1];
+                 date[j+1]:=t;
+
+            end;
+       end;
+  end;
+  for i:=0 to count do
+  begin
+       Memo9.Lines.add(FloatToStr(money[i]));
+       Memo8.Lines.add(IntTostr(date[i]));
+  end;
+
+end;
+
+
 
 procedure TForm1.Button2Click(Sender: TObject);
 var i:integer;
@@ -97,9 +257,16 @@ begin
   Label11.Caption:='';
   Label8.Caption:='';
   Label6.Caption:='';
+  Label16.Caption:='';
   memo1.Clear;
   memo2.clear;
   memo3.clear;
+  memo4.clear;
+  memo5.clear;
+  memo6.clear;
+  memo7.clear;
+  memo8.clear;
+  memo9.clear;
   for i:=0 to countElements-1 do
   begin
       profit[i]:=0;
@@ -179,6 +346,7 @@ begin
            index:=i;
       end;
   end;
+  MaxSpending:=index;
   Label6.Caption:=IntToStr(years[index]);
 end;
 
@@ -224,6 +392,7 @@ end;
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   countElements:=0;
+  memo1.Enabled:=false;
 end;
 
 end.
